1396aba7d2a91c5cabbe05e5fccf1d3ff276a019
Wasana Senevirathna <wus@case.edu>
2017-01-19 10:58:30 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,14 @@
 i = int(input('Enter a number to generate a Fibonacci sequence:-'))
+N = i
 num = 1
 prev2 = 0 
 prev1 = num
-#i = 5
 result = num
-
 while i > 0:
 	i = i - 1
 	result = prev1 + prev2
-	print (result, '=', prev1, '+', prev2)
+	print (result, '=', prev1, '+', prev2) #you can use this print line or the below one
 	#print(result)
 	prev2 = prev1
 	prev1 = result
-print('Done!')
+print('Here is the Fibonacci sequence up to ', N, ' !')
